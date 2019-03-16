9c76ba294c8cd797372da2d3cd90bc810ee222da
Dan Ross <dan.ross@shopify.com>
2019-03-15 23:32:13 -0400

    Remove content from js files

--- src/plugin.js
+++ src/plugin.js
@@ -1,38 +1,5 @@
-/* globals NSWorkspace, NSURL */
-
-import {createWebview, dispatchToWebview} from './utils';
+import {createWebview} from './utils';
 
 export default function(context) {
-  const selectedLayerName = getSelectedLayer(context);
-  const handlers = {
-    reportIssue: () => {
-      const nsurl = NSURL.URLWithString(
-        'https://github.com/Shopify/polaris-icons/issues/new',
-      );
-      NSWorkspace.sharedWorkspace().openURL(nsurl);
-    },
-    appLoaded: () => {
-      checkForSelectedLayer(selectedLayerName);
-    },
-  };
-
-  createWebview(context, handlers, 'Polaris Icons');
-}
-
-function checkForSelectedLayer(selectedLayerName) {
-  if (!selectedLayerName) {
-    return;
-  }
-  dispatchToWebview('SEARCH', String(selectedLayerName), 'onload-sketch');
-}
-
-function getSelectedLayer(context) {
-  const sketchAPI = context.api();
-  const selectedLayers = sketchAPI.selectedDocument.selectedLayers;
-
-  let selectedLayerName;
-  selectedLayers.iterate((layer) => {
-    selectedLayerName = layer.name;
-  });
-  return selectedLayerName;
+  createWebview(context, 'Polaris Icons');
 }
